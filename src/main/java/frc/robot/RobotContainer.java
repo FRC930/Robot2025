f900886e781f525814798e40f5c72eedb1ba7bae
silasnerad <silasnerad@gmail.com>
2025-02-17 17:12:54 -0600

    Adjusted autos and fixed broken stop command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -271,7 +271,7 @@ public class RobotContainer {
     }
 
 
-    autoCommandManager = new AutoCommandManager(drive);
+    autoCommandManager = new AutoCommandManager(drive, shoulder, elbow, elevator, wrist, coralEndEffector, algaeEndEffector);
     reefPositions = ReefPositionsUtil.getInstance();
 
     // Configure the button bindings
