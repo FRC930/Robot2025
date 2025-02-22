0870fa81f07ac11ac48dcbede242f46590f41b84
swegner <steve.wegner@gmail.com>
2025-02-22 13:25:29 -0600

    Added forward driving with strafing - Add driver controls  to strafe based on back of robot (since scoring off back of robot) POV controller (Up back/ Down Forward/Left - right/RIght - back) - Add robot centric driving (DriveCommand.joystickForwardDrive).

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -296,6 +296,30 @@ public class RobotContainer {
             () -> -controller.getRightX() * ANGULAR_SPEED)
           );
 
+          double povSpeed = 1.0;
+          double REVERSE = -1.0;
+          controller
+              .povUp()
+              .whileTrue(
+                  DriveCommands.joystickForwardDrive(
+                      drive, () -> povSpeed * REVERSE, () -> 0.0, null));
+          controller
+              .povDown()
+              .whileTrue(
+                  DriveCommands.joystickForwardDrive(
+                      drive, () -> -(povSpeed * REVERSE), () -> 0.0, null));
+          controller
+              .povRight()
+              .whileTrue(
+                  DriveCommands.joystickForwardDrive(
+                      drive, () -> 0.0, () -> -(povSpeed * REVERSE), null));
+          controller
+              .povLeft()
+              .whileTrue(
+                  DriveCommands.joystickForwardDrive(
+                      drive, () -> 0.0, () -> (povSpeed * REVERSE), null));
+      
+      
     //Auto Align
     // controller.leftStick().onTrue(new RoughAlignToReef(drive,false));
 
