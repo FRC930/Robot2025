7c4dabaf59df67fd7b19a0fe16357f21c621e7a3
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2025-04-12 09:17:14 -0500

    Increased tolerance of Shoulder on L3 and L2 to ensure continuity of auto align

--- src/main/java/frc/robot/commands/StowToL2.java
+++ src/main/java/frc/robot/commands/StowToL2.java
@@ -118,7 +118,7 @@ public class StowToL2 extends SequentialCommandGroup {
     }
 
     public static Trigger getNewAtScoreTrigger(ArmJoint shoulder, ArmJoint elbow, Wrist wrist) {
-        return shoulder.getNewAtAngleTrigger(Degrees.of(ShoulderPositions.Final.position.getAsDouble()), Degrees.of(10.0))
+        return shoulder.getNewAtAngleTrigger(Degrees.of(ShoulderPositions.Final.position.getAsDouble()), Degrees.of(20.0))
             .and(elbow.getNewAtAngleTrigger(Degrees.of(ElbowPositions.Final.position.getAsDouble()), Degrees.of(5.0)))
             .and(wrist.getNewAtAngleTrigger(Degrees.of(WristPositions.Final.position.getAsDouble()), Degrees.of(5.0)));
     }
