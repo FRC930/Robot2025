578c3b7b2971c00e5f4a55c83f8724abd3495046
NickTheFox99 <breakoutboy99@gmail.com>
2025-05-27 19:40:16 -0500

    Complete cleanup of AlgaeEndEffector.java https://docs.google.com/spreadsheets/d/1CWQnYhteYDsKbavCXYH9yAXz-ZcGGPHm5GHMR7VUEIo/edit?gid=0#gid=0&range=37:37

--- src/main/java/frc/robot/subsystems/algaeendeffector/AlgaeEndEffector.java
+++ src/main/java/frc/robot/subsystems/algaeendeffector/AlgaeEndEffector.java
@@ -20,20 +20,21 @@ import frc.robot.subsystems.coralendeffector.CoralEndEffector;
 import frc.robot.util.LoggedTunableNumber;
 
 /**
- * <h1>Algae End Effector</h1>
- * <p>Controls the rollers on the end of our algae end effector</p>
- * <ul>
- * <li>Voltage control</li>
- * </ul>
+ * A {@link edu.wpi.first.wpilibj2.command.Subsystem} to control the roller motors for our Algae End Effector.
+ * Contains an {@link AlgaeEndEffectorIO} to communicate with either the hardware or the simulation.
  */
 public class AlgaeEndEffector extends SubsystemBase {
   public static LoggedTunableNumber ALGAE_DISTANCE_THRESHOLD_HIGH = new LoggedTunableNumber("Sensors/AlgaeEndEffector/SENSORTHRESHOLDHIGH", 4.0);
   public static LoggedTunableNumber ALGAE_DISTANCE_THRESHOLD_LOW = new LoggedTunableNumber("Sensors/AlgaeEndEffector/SENSORTHRESHOLDLOW", 1.7);
-  private AlgaeEndEffectorIO m_IO;
+  private final AlgaeEndEffectorIO m_IO;
 
   AlgaeEndEffectorInputsAutoLogged logged = new AlgaeEndEffectorInputsAutoLogged();
-  private Debouncer algaeDebouncer = new Debouncer(0.15, DebounceType.kBoth);
+  private final Debouncer algaeDebouncer = new Debouncer(0.15, DebounceType.kBoth);
 
+  /**
+   * Constructs an {@link AlgaeEndEffector} with the {@link AlgaeEndEffectorIO} specified.
+   * @param io A specified IO of either {@link AlgaeEndEffectorIONova}, {@link AlgaeEndEffectorIOSim}, or {@link AlgaeEndEffectorIOTalonFX}. Automatically specified when running Sim or Real.
+   */
   public AlgaeEndEffector(AlgaeEndEffectorIO io) {
     m_IO = io;
     logged.angularVelocity = DegreesPerSecond.mutable(0);
@@ -44,22 +45,40 @@ public class AlgaeEndEffector extends SubsystemBase {
     logged.algaeDistance = Inches.mutable(100);
   }
 
+  /**
+   * A function to specify the {@link AlgaeEndEffectorIO} to set the voltage to {@code target}.
+   * @param target The specific {@link Voltage} to set the {@link AlgaeEndEffectorIO}'s target to. The AlgaeEndEffector motor(s) will attempt to reach the set voltage.
+   */
   private void setTarget(Voltage target) {
     m_IO.setTarget(target);
   }
 
+  /**
+   * A factory to return a new {@link InstantCommand} to call {@link AlgaeEndEffector#setTarget}.
+   * @param volts The {@link DoubleSupplier} returning the specific {@link Voltage} in {@link Volts} to set the motor's target to. The DoubleSupplier is called every time the command is called.
+   * @return The constructed {@link InstantCommand}.
+   */
   public Command getNewSetVoltsCommand(DoubleSupplier volts) {
     return new InstantCommand(() -> {
       setTarget(Volts.of((volts.getAsDouble())));
     }, this);
   }
 
+  /**
+   * A factory to return a new {@link InstantCommand} to call {@link AlgaeEndEffector#setTarget}.
+   * @param i The {@link double} specifying the specific {@link Voltage} in {@link Volts} to set the motor's target to. Remember that the double is specified once on command declaration, and not each time the command is run.
+   * @return The constructed {@link InstantCommand}.
+   */
   public Command getNewSetVoltsCommand(double i) {
     return new InstantCommand(() -> {
       setTarget(Volts.of(i));
     }, this);
   }
 
+  /**
+   * A function to decide if there is currently an Algae held in the Algae End Effector, using distance sensors.
+   * @return The {@link boolean} of whether there is currently an Algae in the End Effector.
+   */
   public boolean hasAlgae() {
     return algaeDebouncer.calculate(
       logged.algaeDistance.lte(Inches.of(CoralEndEffector.CORAL_DISTANCE_THRESHOLD_HIGH.get()))
@@ -68,10 +87,18 @@ public class AlgaeEndEffector extends SubsystemBase {
     );
   }
 
+  /**
+   * A factory to return a {@link Trigger} that activates when there is an Algae held in the Algae End Effector. It uses the {@link AlgaeEndEffector#hasAlgae} method.
+   * @return A constructed Trigger activating if the End Effector has an Algae.
+   */
   public Trigger hasAlgaeTrigger() {
     return new Trigger(this::hasAlgae);
   }
 
+  /**
+   * {@inheritDoc}
+   * {@summary Updates all of the LoggedTunableNumbers and AlgaeEndEffectorInputsAutoLogged values.}
+   */
   @Override
   public void periodic() {
     m_IO.updateInputs(logged);
